7285f3301143d131b5616e7192bc2e3b2496b0c9
dersonalfenas <derson.alfenas@gmail.com>
2026-07-22 00:12:47 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.login'
     
     from app import routes, auth
     app.register_blueprint(routes.bp)
