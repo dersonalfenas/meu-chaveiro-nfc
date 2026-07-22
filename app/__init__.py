d808306d5264663dd78dd225732cbe999870dccb
dersonalfenas <derson.alfenas@gmail.com>
2026-07-21 23:51:48 -0300

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+import os
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+    
+    from app import routes, auth
+    app.register_blueprint(routes.bp)
+    app.register_blueprint(auth.bp)
+    
+    return app
