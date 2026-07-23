4dbc7fc8add4f68467e0d50c7bc7a4faa54507fa
dersonalfenas <derson.alfenas@gmail.com>
2026-07-23 00:33:11 -0300

    Delete app directory

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from . import db
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
-class Usuario(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # ... seus campos
