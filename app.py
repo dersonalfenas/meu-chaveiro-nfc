0a8a439d3a377c530e8b5d18b96d06986828fff5
dersonalfenas <derson.alfenas@gmail.com>
2026-07-23 00:05:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-from flask import Flask
-# ... resto das importações
-
-app = Flask(__name__)  # ← TEM QUE TER ISSO!
-
-# ... suas rotas e configurações
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+@app.route('/')
+def index():
+    return render_template('index.html')
