b330bddec0b35570aed31901ed2d72e455c861b9
dersonalfenas <derson.alfenas@gmail.com>
2026-07-23 00:34:17 -0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-secreta')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///nfc.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
